5bf84f38d649a97ca0d7554e1ff86593e03b7a2e
Koichi Kaneshige <coarse.ground@gmail.com>
2024-01-05 13:44:17 +0900

    表示調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def render_errors_for(model)
     render partial: "shared/form_errors", locals: { model: model } if model.errors.present?
   end
+
+  def due_date_format(task)
+    task.due_date.year == Time.current.year ? :short : :default
+  end
 end
