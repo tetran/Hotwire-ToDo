6ea50ae021e0179e002447e681f317c3d38f2efd
Koichi Kaneshige <coarse.ground@gmail.com>
2024-01-24 18:33:24 +0900

    プロジェクト・タスクの権限制御

--- app/controllers/tasks/completes_controller.rb
+++ app/controllers/tasks/completes_controller.rb
@@ -1,7 +1,8 @@
 module Tasks
   class CompletesController < ApplicationController
+    include TaskDependent
+
     def create
-      @task = current_user.tasks.find(params[:task_id])
       @task.complete!
       respond_to do |format|
         format.turbo_stream
@@ -9,7 +10,6 @@ module Tasks
     end
 
     def destroy
-      @task = current_user.tasks.find(params[:task_id])
       @task.uncomplete!
       respond_to do |format|
         format.turbo_stream
