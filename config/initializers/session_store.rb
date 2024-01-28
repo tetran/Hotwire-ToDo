7cb2b93a5638571722d9efa81cf473587a78c627
Koichi Kaneshige <coarse.ground@gmail.com>
2024-01-29 00:03:00 +0900

    Use redis for session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :active_record_store, key: "_hobo_session", expire_after: 90.minutes
+if Rails.env.production?
+  Rails.application.config.session_store :redis_store, servers: "#{ENV.fetch("REDIS_URL")}/0/session", expire_after: 90.minutes
+else
+  Rails.application.config.session_store :active_record_store, key: "_hobo_session", expire_after: 90.minutes
+end
