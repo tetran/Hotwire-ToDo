d3c6ab327f8a947d462e68609d63b424422a239e
Koichi Kaneshige <coarse.ground@gmail.com>
2024-01-29 11:56:13 +0900

    Update config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,17 @@
-if Rails.env.production?
-  Rails.application.config.session_store :redis_store, servers: "#{ENV.fetch("REDIS_URL")}/0/session", expire_after: 90.minutes
-else
-  Rails.application.config.session_store :active_record_store, key: "_hobo_session", expire_after: 90.minutes
+key = "_hobo_session".freeze
+expire_after = 90.minutes.freeze
+begin
+  Rails.application.config.session_store :redis_session_store,
+                                         key: key,
+                                         redis: {
+                                           expire_after: expire_after,
+                                           key_prefix: 'session:',
+                                           url: "#{ENV.fetch("REDIS_URL")}/1",
+                                         }
+rescue => e
+  # When Redis is not available, fallback to ActiveRecord session store
+  Sentry.capture_exception(e)
+  Rails.logger.error("Redis is not available, falling back to ActiveRecord session store\n#{e}")
+
+  Rails.application.config.session_store :active_record_store, key: key, expire_after: expire_after
 end
