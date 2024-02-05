7dc34de0743fdcf2f04a317fb1fd3d81f72c141c
Koichi Kaneshige <coarse.ground@gmail.com>
2024-02-05 22:38:44 +0900

    完了タスク表示

--- app/controllers/tasks/completes_controller.rb
+++ app/controllers/tasks/completes_controller.rb
@@ -1,6 +1,14 @@
 module Tasks
   class CompletesController < ApplicationController
+    include ProjectDependent
     include TaskDependent
+
+    before_action :set_project, only: :index
+    before_action :set_task, only: [:create, :destroy]
+
+    def index
+      @completed_tasks = @project.tasks.completed.with_rich_text_description_and_embeds.order(updated_at: :desc)
+    end
 
     def create
       @task.complete!
