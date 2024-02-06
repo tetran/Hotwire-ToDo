626866d8a8b91732d365494163e09e657fd9256f
Koichi Kaneshige <coarse.ground@gmail.com>
2024-02-06 22:04:34 +0900

    リンク配置

--- app/controllers/tasks/completes_controller.rb
+++ app/controllers/tasks/completes_controller.rb
@@ -16,12 +16,5 @@ module Tasks
         format.turbo_stream
       end
     end
-
-    def destroy
-      @task.uncomplete!
-      respond_to do |format|
-        format.turbo_stream
-      end
-    end
   end
 end
